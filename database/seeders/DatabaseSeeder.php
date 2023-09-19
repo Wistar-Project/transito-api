baef7e4e862b975f32c43682549b47a3dc3d815b
fabrisdev <ezepuntocom@hotmail.com>
2023-09-19 14:58:34 -0300

    Seed para segundo test listo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,10 @@ namespace Database\Seeders;
 use App\Models\Alojamiento;
 use Illuminate\Database\Seeder;
 use App\Models\Paquete;
+use App\Models\PaqueteAsignadoAPickup;
+use App\Models\Pickup;
 use App\Models\Sede;
+use App\Models\Vehiculo;
 
 class DatabaseSeeder extends Seeder
 {
@@ -29,5 +32,24 @@ class DatabaseSeeder extends Seeder
             "email" => "prueba@email.com",
             "destino" => 1
         ]);
+
+        Paquete::create([
+            "id" => 2,
+            "peso_en_kg" => 5,
+            "email" => "prueba@email.com",
+            "destino" => 1
+        ]);
+        Vehiculo::create([
+            "id" => 1,
+            "capacidad_en_toneladas" => 5
+        ]);
+        Pickup::create([
+            "id_vehiculo" => 1
+        ]);
+        PaqueteAsignadoAPickup::create([
+            "id_paquete" => 2,
+            "id_pickup" => 1
+        ]);
+
     }
 }

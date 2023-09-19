9c8af3493984b44065cb23f4429d63dbb02d26c7
fabrisdev <ezepuntocom@hotmail.com>
2023-09-19 15:55:44 -0300

    Seed para tercer test listo

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,15 @@
 namespace Database\Seeders;
 
 use App\Models\Alojamiento;
+use App\Models\Conductor;
+use App\Models\ConductorManeja;
 use Illuminate\Database\Seeder;
 use App\Models\Paquete;
 use App\Models\PaqueteAsignadoAPickup;
+use App\Models\Persona;
 use App\Models\Pickup;
 use App\Models\Sede;
+use App\Models\User;
 use App\Models\Vehiculo;
 
 class DatabaseSeeder extends Seeder
@@ -51,5 +55,37 @@ class DatabaseSeeder extends Seeder
             "id_pickup" => 1
         ]);
 
+        Paquete::create([
+            "id" => 3,
+            "peso_en_kg" => 5,
+            "email" => "prueba@email.com",
+            "destino" => 1
+        ]);
+        Vehiculo::create([
+            "id" => 2,
+            "capacidad_en_toneladas" => 5
+        ]);
+        Pickup::create([
+            "id_vehiculo" => 2
+        ]);
+        PaqueteAsignadoAPickup::create([
+            "id_paquete" => 3,
+            "id_pickup" => 2
+        ]);
+        User::factory([
+            "id" => 1
+        ]) -> create();
+        Persona::create([
+            "id" => 1,
+            "nombre" => "Una",
+            "apellido" => "Prueba"
+        ]);
+        Conductor::create([
+            "id" => 1
+        ]);
+        ConductorManeja::create([
+            "id_conductor" => 1,
+            "id_vehiculo" => 2
+        ]);
     }
 }

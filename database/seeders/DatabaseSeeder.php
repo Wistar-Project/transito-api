a4220ee5bd07396725070a8a637371cecb98cf5b
fabrisdev <ezepuntocom@hotmail.com>
2023-09-19 21:12:01 -0300

    Seed para último test de paquetes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,12 @@
 namespace Database\Seeders;
 
 use App\Models\Alojamiento;
+use App\Models\Camion;
 use App\Models\Conductor;
 use App\Models\ConductorManeja;
+use App\Models\Lote;
+use App\Models\LoteAsignadoACamion;
+use App\Models\LoteFormadoPor;
 use Illuminate\Database\Seeder;
 use App\Models\Paquete;
 use App\Models\PaqueteAsignadoAPickup;
@@ -86,6 +90,47 @@ class DatabaseSeeder extends Seeder
         ConductorManeja::create([
             "id_conductor" => 1,
             "id_vehiculo" => 2
+        ]);
+
+        Lote::create([
+            "id" => 1,
+            "destino" => 1
+        ]);
+        Paquete::create([
+            "id" => 4,
+            "peso_en_kg" => 5,
+            "email" => "prueba@email.com",
+            "destino" => 1
+        ]);
+        LoteFormadoPor::create([
+            "id_paquete" => 4,
+            "id_lote" => 1
+        ]);
+        Vehiculo::create([
+            "id" => 3,
+            "capacidad_en_toneladas" => 5
+        ]);
+        Camion::create([
+            "id_vehiculo" => 3
+        ]);
+        LoteAsignadoACamion::create([
+            "id_lote" => 1,
+            "id_camion" => 3
+        ]);
+        User::factory([
+            "id" => 2
+        ]) -> create();
+        Persona::create([
+            "id" => 2,
+            "nombre" => "Otra",
+            "apellido" => "Persona"
+        ]);
+        Conductor::create([
+            "id" => 2
+        ]);
+        ConductorManeja::create([
+            "id_conductor" => 2,
+            "id_vehiculo" => 3
         ]);
     }
 }

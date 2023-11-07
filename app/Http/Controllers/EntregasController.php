4f3d1044855991ddad61dfff16452baeb8c41299
fabrisdev <ezepuntocom@hotmail.com>
2023-11-07 14:07:35 -0300

    Ahora muestra lotes o paquetes a bajar

--- app/Http/Controllers/EntregasController.php
+++ app/Http/Controllers/EntregasController.php
@@ -2,9 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Alojamiento;
+use App\Models\ConductorManeja;
+use App\Models\Lote;
+use App\Models\LoteAsignadoACamion;
+use App\Models\PaqueteAsignadoAPickup;
 use Illuminate\Http\Request;
 
 class EntregasController extends Controller
 {
-    //
+
+    public function MostrarDescarga(Request $request, $direccion)
+    {
+        $vehiculoAsignado = ConductorManeja::findOrFail("id_conductor", $request->user()->user_id);
+        if($vehiculoAsignado ->tipo->tipo == "camión")
+            return $this->descargaDeLotes($vehiculoAsignado->id_vehiculo, $direccion);
+        return $this -> descargaDePaquetes($vehiculoAsignado->id_vehiculo, $direccion);
+    }
+
+    private function descargaDeLotes($idCamion, $direccion){
+        $lotesAsignados = LoteAsignadoACamion::where('id_camion', $idCamion) -> get();
+        return array_filter($lotesAsignados, function($loteAsignado) use($direccion){
+            return $loteAsignado -> lote -> alojamiento -> direccion == $direccion;
+        }) -> pluck("id_lote");
+    }
+
+    private function descargaDePaquetes($idPickup, $direccion){
+        $paquetesAsignados = PaqueteAsignadoAPickup::where('id_pickup', $idPickup) -> get();
+        return array_filter($paquetesAsignados, function($paqueteAsignado) use($direccion){
+            return $paqueteAsignado -> paquete -> alojamiento -> direccion == $direccion;
+        }) -> pluck("id_pickup");
+    }
+
 }

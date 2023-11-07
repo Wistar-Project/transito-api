bcdba34f5a0fe5f9ba24f486be1a6231e9adff9c
fabrisdev <ezepuntocom@hotmail.com>
2023-11-07 20:58:30 -0300

    soft deletes agregado

--- app/Models/LoteAsignadoACamion.php
+++ app/Models/LoteAsignadoACamion.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LoteAsignadoACamion extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
     protected $primaryKey = "id_lote";
     protected $table = "lote_asignado_a_camion";
     protected $fillable = [

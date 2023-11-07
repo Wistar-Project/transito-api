723ba37ff93f5bbff1e859b0970c8a165580d93b
fabrisdev <ezepuntocom@hotmail.com>
2023-11-07 15:12:37 -0300

    Descargas funcionando correctamente

--- app/Http/Controllers/EntregasController.php
+++ app/Http/Controllers/EntregasController.php
@@ -12,26 +12,35 @@ use Illuminate\Http\Request;
 class EntregasController extends Controller
 {
 
-    public function MostrarDescarga(Request $request, $direccion)
+    public function MostrarDescarga(Request $request, $idSede)
     {
-        $vehiculoAsignado = ConductorManeja::findOrFail("id_conductor", $request->user()->user_id);
-        if($vehiculoAsignado ->tipo->tipo == "camión")
-            return $this->descargaDeLotes($vehiculoAsignado->id_vehiculo, $direccion);
-        return $this -> descargaDePaquetes($vehiculoAsignado->id_vehiculo, $direccion);
+        $vehiculoAsignado = ConductorManeja::findOrFail($request->user()->id);
+        if ($vehiculoAsignado->tipo->tipo == "camión")
+            return $this->descargaDeLotes($vehiculoAsignado->id_vehiculo, $idSede);
+        return $this->descargaDePaquetes($vehiculoAsignado->id_vehiculo, $idSede);
     }
 
-    private function descargaDeLotes($idCamion, $direccion){
-        $lotesAsignados = LoteAsignadoACamion::where('id_camion', $idCamion) -> get();
-        return array_filter($lotesAsignados, function($loteAsignado) use($direccion){
-            return $loteAsignado -> lote -> alojamiento -> direccion == $direccion;
-        }) -> pluck("id_lote");
+    private function descargaDeLotes($idCamion, $idSede)
+    {
+        $lotesAsignados = LoteAsignadoACamion::where('id_camion', $idCamion)->get();
+
+        $lotesAsignadosConEseDestino = [];
+        foreach ($lotesAsignados as $loteAsignado) {
+            if ($loteAsignado->lote->destino == $idSede)
+                array_push($lotesAsignadosConEseDestino, $loteAsignado->id_lote);
+        }
+        return $lotesAsignadosConEseDestino;
     }
 
-    private function descargaDePaquetes($idPickup, $direccion){
-        $paquetesAsignados = PaqueteAsignadoAPickup::where('id_pickup', $idPickup) -> get();
-        return array_filter($paquetesAsignados, function($paqueteAsignado) use($direccion){
-            return $paqueteAsignado -> paquete -> alojamiento -> direccion == $direccion;
-        }) -> pluck("id_pickup");
+    private function descargaDePaquetes($idPickup, $idSede)
+    {
+        $paquetesAsignados = PaqueteAsignadoAPickup::where('id_pickup', $idPickup)->get();
+        $paquetesConEseDestino = [];
+        foreach ($paquetesAsignados as $paqueteAsignado) {
+            if ($paqueteAsignado->paquete->alojamiento->id == $idSede)
+                array_push($paquetesConEseDestino, $paqueteAsignado->id_paquete);
+        }
+        return $paquetesConEseDestino;
     }
 
 }

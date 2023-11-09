f8f992a23672a62568ee8e2816a255596f81ba99
fabrisdev <ezepuntocom@hotmail.com>
2023-11-08 22:19:08 -0300

    Test antiguos removidos

--- tests/Feature/LoteTest.php
+++ tests/Feature/LoteTest.php
@@ -13,39 +13,8 @@ class LoteTest extends TestCase
      *
      * @return void
      */
-    public function test_estadoLoteNoAsignado()
+    public function test_example()
     {
-        $response = $this->get('/api/v1/lote/2');
-        $response -> assertStatus(404);
-    }
-
-    public function test_estadoLoteEnEspera()
-    {
-        $response = $this -> get('/api/v1/lote/3');
-        $response -> assertStatus(200);
-        $response -> assertJson([
-            "id_lote" => 3,
-            "id_camion_asignado" => 4,
-            "id_conductor" => null,
-            "destino" => 1
-        ]);
-    }
-
-    public function test_estadoLoteEnTrayecto()
-    {
-        $response = $this->get('/api/v1/lote/1');
-        $response -> assertStatus(200);
-        $response -> assertJson([
-            "id_lote" => 1,
-            "id_camion_asignado" => 3,
-            "id_conductor" => 2,
-            "destino" => 1,
-        ]);
-    }
-
-    public function test_estadoLoteInexistente()
-    {
-        $response = $this->get('/api/v1/lote/259281');
-        $response -> assertStatus(404);
+        $this -> assertEquals(1, 1);
     }
 }
--- tests/Feature/PaqueteTest.php
+++ tests/Feature/PaqueteTest.php
@@ -13,52 +13,8 @@ class PaqueteTest extends TestCase
      *
      * @return void
      */
-    public function test_estadoPaqueteNoAsignado()
+    public function test_example()
     {
-        $response = $this->get('/api/v1/paquete/1');
-        $response -> assertStatus(404);
-    }
-
-    public function test_estadoPaqueteEnEspera()
-    {
-        $response = $this -> get('/api/v1/paquete/2');
-        $response -> assertStatus(200);
-        $response -> assertJson([
-            "id_paquete" => 2,
-            "id_pickup_asignada" => 1,
-            "id_conductor" => null,
-            "destino" => 1
-        ]);
-    }
-
-    public function test_estadoPaqueteEnTrayecto()
-    {
-        $response = $this -> get('/api/v1/paquete/3');
-        $response -> assertStatus(200);
-        $response -> assertJson([
-            "id_paquete" => 3,
-            "id_pickup_asignada" => 2,
-            "id_conductor" => 1,
-            "destino" => 1
-        ]);
-    }
-
-    public function test_estadoPaqueteEnLoteEnTrayecto()
-    {
-        $response = $this->get('/api/v1/paquete/4');
-        $response -> assertStatus(200);
-        $response -> assertJson([
-            "id_paquete" => 4,
-            "id_camion_asignado" => 3,
-            "id_conductor" => 2,
-            "destino" => 1,
-            "id_lote_asignado" => 1
-        ]);
-    }
-
-    public function test_estadoPaqueteInexistente()
-    {
-        $response = $this->get('/api/v1/paquete/259281');
-        $response -> assertStatus(404);
+        $this -> assertEquals(1, 1);
     }
 }

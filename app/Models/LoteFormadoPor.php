f36fbf880d7928c4f708feb8ba07bc4fd75782b1
fabrisdev <ezepuntocom@hotmail.com>
2023-11-10 19:26:58 -0300

    Seguimiento de lotes funcionando

--- app/Models/LoteFormadoPor.php
+++ app/Models/LoteFormadoPor.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LoteFormadoPor extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
     protected $table = "lote_formado_por";
     protected $primaryKey = "id_paquete";
     protected $fillable = [

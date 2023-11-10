99536efbad91c75a16a84238f5edc1006dd883d5
fabrisdev <ezepuntocom@hotmail.com>
2023-11-10 19:46:57 -0300

    Nuevos tests definidos

--- tests/Feature/LoteTest.php
+++ tests/Feature/LoteTest.php
@@ -2,6 +2,10 @@
 
 namespace Tests\Feature;
 
+use App\Models\Alojamiento;
+use App\Models\Lote;
+use App\Models\Sede;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -15,6 +19,21 @@ class LoteTest extends TestCase
      */
     public function test_estado()
     {
+        $user = User::factory()->create();
+        Alojamiento::create(["id" => 1, "direccion" => "Una dirección"]);
+        Lote::create(["id" => 1, "destino" => 1]);
+        Sede::create(["id" => 1]);
+        $this -> actingAs($user) -> post('/api/v1/lote/1');
+
+    }
+
+    public function test_estadoSinAsignarVehiculo()
+    {
+        $this -> assertEquals(1, 1);
+    }
+
+    public function test_estadoSinAsignarChofer()
+    {
         $this -> assertEquals(1, 1);
     }
 
@@ -24,6 +43,16 @@ class LoteTest extends TestCase
     }
 
     public function test_estadoSinAutenticarse()
+    {
+        $this -> assertEquals(1, 1);
+    }
+
+    public function test_estadoSiendoFuncionario()
+    {
+        $this -> assertEquals(1, 1);
+    }
+
+    public function test_estadoSiendoChofer()
     {
         $this -> assertEquals(1, 1);
     }

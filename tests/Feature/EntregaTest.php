69ce38b9d65caff6067a1c2b089b45c200e88c97
fabrisdev <ezepuntocom@hotmail.com>
2023-11-11 16:05:13 -0300

    Tests de descargas listos

--- tests/Feature/EntregaTest.php
+++ tests/Feature/EntregaTest.php
@@ -170,4 +170,113 @@ class EntregaTest extends TestCase
             "message" => "Unauthenticated."
         ]);
     }
+
+    public function test_mostrar_descargas_en_camion()
+    {
+        $conductor = $this -> crearConductor();
+        Alojamiento::create(["id" => 35, "direccion" => "Dirección 35"]);
+        Sede::create(["id" => 35]);
+        Vehiculo::create(["id" => 35, "capacidad_en_toneladas" => 5]);
+        Camion::create(["id_vehiculo" => 35]);
+        ConductorManeja::create(["id_vehiculo" => 35, "id_conductor" => $conductor -> id]);
+        Lote::create(["id" => 35, "destino" => 35]);
+        Lote::create(["id" => 36, "destino" => 35]);
+        Lote::create(["id" => 37, "destino" => 35]);
+        LoteAsignadoACamion::create(["id_lote" => 35, "id_camion" => 35]);
+        LoteAsignadoACamion::create(["id_lote" => 36, "id_camion" => 35]);
+        LoteAsignadoACamion::create(["id_lote" => 37, "id_camion" => 35]);
+
+        $response = $this->actingAs($conductor)->get('/api/v1/entregas/35');
+        $response->assertStatus(200);
+        $response->assertExactJson([ 35, 36, 37 ]);
+    }
+
+    public function test_mostrar_descargas_en_pickup()
+    {
+        $conductor = $this -> crearConductor();
+        Alojamiento::create(["id" => 36, "direccion" => "Dirección 36"]);
+        Sede::create(["id" => 36]);
+        Vehiculo::create(["id" => 36, "capacidad_en_toneladas" => 5]);
+        Pickup::create(["id_vehiculo" => 36]);
+        ConductorManeja::create(["id_vehiculo" => 36, "id_conductor" => $conductor -> id]);
+        Paquete::create(["id" => 40, "destino" => 36, "peso_en_kg" => 5, "email" => "a@gmail.com"]);
+        Paquete::create(["id" => 41, "destino" => 36, "peso_en_kg" => 5, "email" => "a@gmail.com"]);
+        Paquete::create(["id" => 42, "destino" => 36, "peso_en_kg" => 5, "email" => "a@gmail.com"]);
+        PaqueteAsignadoAPickup::create(["id_paquete" => 40, "id_pickup" => 36]);
+        PaqueteAsignadoAPickup::create(["id_paquete" => 41, "id_pickup" => 36]);
+        PaqueteAsignadoAPickup::create(["id_paquete" => 42, "id_pickup" => 36]);
+
+        $response = $this->actingAs($conductor)->get('/api/v1/entregas/36');
+        $response->assertStatus(200);
+        $response->assertExactJson([ 40, 41, 42 ]);
+    }
+
+    public function test_mostrar_descargas_sin_vehiculo()
+    {
+        $conductor = $this -> crearConductor();
+        Alojamiento::create(["id" => 37, "direccion" => "Dirección 37"]);
+        Sede::create(["id" => 37]);
+
+        $response = $this->actingAs($conductor)->get('/api/v1/entregas/37');
+        $response->assertStatus(404);
+    }
+
+    public function test_mostrar_descargas_sin_autenticarse()
+    {
+        Alojamiento::create(["id" => 38, "direccion" => "Dirección 38"]);
+        Sede::create(["id" => 38]);
+
+        $response = $this->get('/api/v1/entregas/38', [
+            "Accept" => "application/json"
+        ]);
+        $response->assertStatus(401);
+        $response->assertExactJson([
+            "message" => "Unauthenticated."
+        ]);
+    }
+
+    public function test_mostrar_descargas_siendo_administrador()
+    {
+        $administrador = User::factory()->create();
+        Persona::create(["id" => $administrador->id, "nombre" => "Domingo", "apellido" => "Perez"]);
+        Administrador::create(["id" => $administrador -> id]);
+        Alojamiento::create(["id" => 39, "direccion" => "Dirección 39"]);
+        Sede::create(["id" => 39]);
+
+        $response = $this->actingAs($administrador)->get('/api/v1/entregas/39');
+        $response->assertStatus(401);
+        $response->assertExactJson([
+            "message" => "No tienes permiso para ver esto."
+        ]);
+    }
+
+    public function test_mostrar_descargas_siendo_gerente()
+    {
+        $gerente = User::factory()->create();
+        Persona::create(["id" => $gerente->id, "nombre" => "Domingo", "apellido" => "Perez"]);
+        Gerente::create(["id" => $gerente -> id]);
+        Alojamiento::create(["id" => 40, "direccion" => "Dirección 40"]);
+        Sede::create(["id" => 40]);
+
+        $response = $this->actingAs($gerente)->get('/api/v1/entregas/40');
+        $response->assertStatus(401);
+        $response->assertExactJson([
+            "message" => "No tienes permiso para ver esto."
+        ]);
+    }
+
+    public function test_mostrar_descargas_siendo_funcionario()
+    {
+        $funcionario = User::factory()->create();
+        Persona::create(["id" => $funcionario->id, "nombre" => "Domingo", "apellido" => "Perez"]);
+        Funcionario::create(["id" => $funcionario -> id]);
+        Alojamiento::create(["id" => 41, "direccion" => "Dirección 41"]);
+        Sede::create(["id" => 41]);
+
+        $response = $this->actingAs($funcionario)->get('/api/v1/entregas/41');
+        $response->assertStatus(401);
+        $response->assertExactJson([
+            "message" => "No tienes permiso para ver esto."
+        ]);
+    }
 }

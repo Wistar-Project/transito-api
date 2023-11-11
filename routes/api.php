37fa9ab76e8d1da2b43aeb1f29ccf8d4a6968314
fabrisdev <ezepuntocom@hotmail.com>
2023-11-10 22:44:25 -0300

    Usando middlewares

--- routes/api.php
+++ routes/api.php
@@ -23,10 +23,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::prefix('v1')->group(function () {
     Route::middleware('auth:api')->group(function(){
-        Route::get('/paquete/{d}', [PaqueteController::class, "ObtenerEstado"]);
-        Route::get('/lote/{d}', [LoteController::class, "ObtenerEstado"]);
-        Route::get('/entregas', [EntregasController::class, "Mostrar"]);
-        Route::get('/entregas/{d}', [EntregasController::class, "MostrarDescarga"]);
-        Route::delete('/entregas/{d}', [EntregasController::class, "MarcarEntregada"]);
+        Route::get('/paquete/{d}', [PaqueteController::class, "ObtenerEstado"]) -> middleware('admin-o-gerente');
+        Route::get('/lote/{d}', [LoteController::class, "ObtenerEstado"]) -> middleware('admin-o-gerente');
+        Route::get('/entregas', [EntregasController::class, "Mostrar"]) -> middleware('chofer-o-mayor');
+        Route::get('/entregas/{d}', [EntregasController::class, "MostrarDescarga"]) -> middleware('chofer-o-mayor');
+        Route::delete('/entregas/{d}', [EntregasController::class, "MarcarEntregada"]) -> middleware('chofer-o-mayor');
     });
 });
